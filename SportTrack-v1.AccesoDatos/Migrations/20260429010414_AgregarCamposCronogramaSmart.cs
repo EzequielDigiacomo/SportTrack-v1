f1461434814b7e9ebd37daac7971b9565ec1e104
Ezequiel Di Giacomo <ezequiel250891@gmail.com>
2026-04-28 22:30:52 -0300

    migraciones

---
+++ SportTrack-v1.AccesoDatos/Migrations/20260429010414_AgregarCamposCronogramaSmart.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportTrack_v1.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarCamposCronogramaSmart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CarrilesDisponibles",
+                schema: "regatas",
+                table: "Eventos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "HoraFinReceso",
+                schema: "regatas",
+                table: "Eventos",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "HoraInicioEvento",
+                schema: "regatas",
+                table: "Eventos",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "HoraInicioReceso",
+                schema: "regatas",
+                table: "Eventos",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<int>(
+                name: "PerfilTiempo",
+                schema: "regatas",
+                table: "Eventos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PermitirCombinadas",
+                schema: "regatas",
+                table: "Eventos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CarrilesDisponibles",
+                schema: "regatas",
+                table: "Eventos");
+
+            migrationBuilder.DropColumn(
+                name: "HoraFinReceso",
+                schema: "regatas",
+                table: "Eventos");
+
+            migrationBuilder.DropColumn(
+                name: "HoraInicioEvento",
+                schema: "regatas",
+                table: "Eventos");
+
+            migrationBuilder.DropColumn(
+                name: "HoraInicioReceso",
+                schema: "regatas",
+                table: "Eventos");
+
+            migrationBuilder.DropColumn(
+                name: "PerfilTiempo",
+                schema: "regatas",
+                table: "Eventos");
+
+            migrationBuilder.DropColumn(
+                name: "PermitirCombinadas",
+                schema: "regatas",
+                table: "Eventos");
+        }
+    }
+}
