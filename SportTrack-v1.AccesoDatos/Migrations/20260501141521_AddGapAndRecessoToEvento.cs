1ae8ad5e3ffc77c715a0f48a6aa7479a2fc801c8
Ezequiel Di Giacomo <ezequiel250891@gmail.com>
2026-05-01 11:15:57 -0300

    migraciones aplicadas a render

---
+++ SportTrack-v1.AccesoDatos/Migrations/20260501141521_AddGapAndRecessoToEvento.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportTrack_v1.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGapAndRecessoToEvento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GapEntrePruebas",
+                schema: "regatas",
+                table: "Eventos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SinReceso",
+                schema: "regatas",
+                table: "Eventos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TimeZoneId",
+                schema: "regatas",
+                table: "Eventos",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GapEntrePruebas",
+                schema: "regatas",
+                table: "Eventos");
+
+            migrationBuilder.DropColumn(
+                name: "SinReceso",
+                schema: "regatas",
+                table: "Eventos");
+
+            migrationBuilder.DropColumn(
+                name: "TimeZoneId",
+                schema: "regatas",
+                table: "Eventos");
+        }
+    }
+}
